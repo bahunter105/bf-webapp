b4b481cb8430300e861eef6ad41b352cdc8fae0e
Hunter Bourgeois <bahunter@gmail.com>
2022-08-10 19:28:04 +0200

    basic outline of workshop page done

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -10,12 +10,14 @@ class WorkshopsController < ApplicationController
     #   end
     # end
     moodle_workshops.each do |workshop|
-      if (workshop["id"] != 0 && Workshop.find_by(moodleid:workshop["id"]).nil?)
+      if (workshop["id"] != 1 && Workshop.find_by(moodleid:workshop["id"]).nil?)
         new_workshop = Workshop.new
         new_workshop.fullname = workshop["fullname"]
         new_workshop.shortname = workshop["shortname"]
-        new_workshop.summary = workshop["summary"]
+        summary_string = workshop["summary"].match(/>.+</).to_s
+        new_workshop.summary = summary_string[1...-1]
         new_workshop.language = workshop["lang"]
+        new_workshop.moodleid = workshop["id"]
         new_workshop.save
       end
     end
