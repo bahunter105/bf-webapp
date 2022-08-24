7c35333aab9a804192f41223d5d6bea9bec7ce34
Hunter Bourgeois <bahunter@gmail.com>
2022-08-24 11:37:22 +0200

    updated seed file and rails controller

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -2,33 +2,39 @@ class WorkshopsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def workshops
-    moodle = MoodleRb.new(ENV["MOODLE_WEBSERVICES_TOKEN"], ENV["MOODLE_URL"])
+    moodle = MoodleRb.new(ENV['MOODLE_WEBSERVICES_TOKEN'], ENV['MOODLE_URL'])
     moodle_workshops = moodle.courses.index
-    # moodle_workshops.each do |workshop|
-    #   if (workshop["id"] != 0 && Workshop.find_by(moodleid:workshop["id"]).nil?)
-    #     p workshop["id"]
-    #   end
-    # end
+
     moodle_workshops.each do |workshop|
-      if (workshop["id"] != 1 && Workshop.find_by(moodleid:workshop["id"]).nil?)
-        new_workshop = Workshop.new
-        new_workshop.fullname = workshop["fullname"]
-        new_workshop.shortname = workshop["shortname"]
-        summary_string = workshop["summary"].match(/>.+</).to_s
-        new_workshop.summary = summary_string[1...-1]
-        new_workshop.language = workshop["lang"]
-        new_workshop.moodleid = workshop["id"]
-        new_workshop.save
+      next if workshop['id'] == 1
+      next if Workshop.find_by(moodleid: workshop['id'])
+
+      new_workshop = Workshop.new
+      new_workshop.fullname = workshop['fullname']
+      new_workshop.shortname = workshop['shortname']
+      new_workshop.language = workshop['lang']
+      new_workshop.moodleid = workshop['id']
+
+      summary_string = workshop['summary'].match(/>.+</).to_s
+      new_workshop.summary = summary_string[1...-1]
+
+      case workshop['categoryid']
+      when 1
+        new_workshop.category = 'families'
+      when 2
+        new_workshop.category = 'educators'
+      else
+        new_workshop.category = 'families'
       end
+
+      new_workshop.save
     end
 
     @q = Workshop.ransack(params[:q])
     @workshops = @q.result(distinct: true)
-
   end
 
   def show
     @workshop = Workshop.find(params[:id])
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+User.destroy_all
+Workshop.destroy_all
+
+puts "Creating users..."
+user1 = {email: "bob@gmail.cs", password: '123456'}
+user2 = {email: "jim@gmail.cs", password: '123456'}
+
+[user1, user2].each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.email}"
+end
+puts "Finished!"
+
+puts "Creating workshops..."
+ws1 = {fullname: 'DB Test WS ', shortname: 'DB Test', summary: 'An Awesome Test', language: 'English', category: "families"}
+ws2 = {fullname: 'DB Seed WS ', shortname: 'DB Seeding', summary: 'DB Seeding', language: 'English', category: "educators"}
+
+[ws1, ws2].each do |attributes|
+  ws = Workshop.create!(attributes)
+  puts "Created #{ws.shortname}"
+end
+puts "Finished!"
