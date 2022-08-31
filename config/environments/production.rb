6089ddce9dc38e268966c83fca15de63ee64540b
Hunter Bourgeois <bahunter@gmail.com>
2022-08-31 12:35:56 -0500

    changed mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "bf_webapp_production"
 
+  # Devise mailer setup
+  config.action_mailer.default_url_options = { host: 'www.brightfutures.net'}
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
