d17def792db4a600d6b688f964763c9200cd64a9
Hunter Bourgeois <bahunter@gmail.com>
2022-09-02 13:56:56 -0500

    cookies working again

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,4 +1,6 @@
 class CookiesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
   def index
     session[:cookies_accepted] = params[:cookies] if params[:cookies]
   end
