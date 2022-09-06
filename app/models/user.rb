1e4e59ae10a48f1ef5f246270449821695eb73a8
Hunter Bourgeois <bahunter@gmail.com>
2022-09-06 12:00:39 -0500

    action mailer setup for welcome email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :workshops, through: :bookmarks
   validates :first_name, :last_name, presence: true
+
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome.deliver_now!
+  end
 end
